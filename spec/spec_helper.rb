21ee0ecb71a8d4b85334be044098289bc303ac58
Matthias Viehweger <kronn@kronn.de>
2014-08-26 17:03:05 +0200

    Switch off simplecov on Ruby 1.8.7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter "/spec/"
+if RUBY_VERSION > '1.8.7'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
 end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
