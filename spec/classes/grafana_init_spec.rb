69dff251a745a5e1edce371abce3baa835dd1683
Matthias Viehweger <kronn@kronn.de>
2014-08-26 15:30:51 +0200

    Make spec more error-resitant

--- spec/classes/grafana_init_spec.rb
+++ spec/classes/grafana_init_spec.rb
@@ -4,10 +4,10 @@ describe 'grafana', :type => 'class' do
   context 'with default settings' do
     let :params do
       {
-        :version    => '1.6.1',
+        :version      => '1.6.1',
         :install_dir  => '/opt',
-        :symlink    => true,
-        :symlink_name => '/opt/grafana'
+        :symlink      => true,
+        :symlink_name => '/opt/grafana',
       }
     end
 
@@ -21,18 +21,20 @@ describe 'grafana', :type => 'class' do
     )}
   end
 
-  context 'when a non-default installation directory is specified' do
+  context 'with a non-default installation directory specified' do
     let :params do
       {
-        :install_dir  => '/usr/local'
+        :install_dir  => '/usr/local',
+        :version      => '1.3.3.7',
+        :symlink      => true,
       }
     end
 
-    it { should contain_file('/usr/local/grafana-1.6.1/config.js') }
+    it { should contain_file('/usr/local/grafana-1.3.3.7/config.js') }
 
     it { should contain_file('/usr/local/grafana').with(
       :ensure => 'link',
-      :target => '/usr/local/grafana-1.6.1'
+      :target => '/usr/local/grafana-1.3.3.7'
     )}
   end
 end
