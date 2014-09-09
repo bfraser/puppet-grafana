2281dbaef6e56a931b787f2444f512f7db50b434
Bill Fraser <fraser@pythian.com>
2014-09-09 16:00:52 -0400

    Reinstate default datasources and ignore quoted_boolean lint check

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,11 +3,23 @@
 # Default parameters
 #
 class grafana::params {
-    $version            = '1.7.0'
-    $install_method     = 'archive'
-    $install_dir        = '/opt'
-    $symlink            = false
-    $grafana_user       = 'root'
-    $grafana_group      = 'root'
-    $datasources        = {}
+  $version            = '1.7.0'
+  $install_method     = 'archive'
+  $install_dir        = '/opt'
+  $symlink            = false
+  $grafana_user       = 'root'
+  $grafana_group      = 'root'
+  $datasources        = {
+    'graphite' => {
+      'type'    => 'graphite',
+      'url'     => 'http://localhost:80',
+      'default' => 'true' # lint:ignore:quoted_booleans
+    },
+    'elasticsearch' => {
+      'type'      => 'elasticsearch',
+      'url'       => 'http://localhost:9200',
+      'index'     => 'grafana-dash',
+      'grafanaDB' => 'true' # lint:ignore:quoted_booleans
+    },
+  }
 }
