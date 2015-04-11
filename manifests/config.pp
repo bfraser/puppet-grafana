b391538a2398005654ff405584ab23cfa1774610
Bill Fraser <fraser@pythian.com>
2015-04-11 02:50:32 -0400

    Add logic to control creation of config file for docker container

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,11 +3,22 @@
 # This class is called from grafana
 #
 class grafana::config {
+  if $::grafana::install_method =~ /archive|package/ {
+    $cfg = $::grafana::cfg
 
-  $cfg = $::grafana::cfg
+    file {  $::grafana::cfg_location:
+      ensure  => present,
+      content => template('grafana/config.ini.erb'),
+    }
+  }
+  elsif $::grafana::install_method == 'docker' {
+    if $::grafana::container_cfg {
+      $cfg = $::grafana::cfg
 
-  file {  $::grafana::cfg_location:
-    ensure  => present,
-    content => template('grafana/config.ini.erb'),
+      file {  $::grafana::cfg_location:
+        ensure  => present,
+        content => template('grafana/config.ini.erb'),
+      }
+    }
   }
 }
