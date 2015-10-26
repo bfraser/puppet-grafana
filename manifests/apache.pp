90e4fd010851ebe12fdc46e223cafd444f114862
Cristian Falcas <falcas@optymyze.com>
2015-10-26 10:46:48 +0200

    allow overwrites for vhost_servername and vhost_port

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -3,14 +3,16 @@
 # This class is called from grafana
 #
 class grafana::apache (
-  $server_addr = $grafana::cfg[server][http_addr],
-  $server_port = $grafana::cfg[server][http_port]
+  $server_addr      = $grafana::cfg[server][http_addr],
+  $server_port      = $grafana::cfg[server][http_port],
+  $vhost_servername = $::fqdn,
+  $vhost_port       = 80,
 ) {
   include ::apache
 
   apache::vhost { 'grafana_proxy':
-    servername          => $::fqdn,
-    port                => 80,
+    servername          => $vhost_servername,
+    port                => $vhost_port,
     docroot             => '/var/www/',
     proxy_preserve_host => true,
     proxy_pass          => {
