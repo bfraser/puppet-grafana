aa6dcec7b6c6c398d95aa1d16871b8a3eb68c041
Tim Meusel <tim@bastelfreak.de>
2017-03-12 20:38:32 +0100

    increase spec coverage

--- spec/classes/grafana_spec.rb
+++ spec/classes/grafana_spec.rb
@@ -26,6 +26,7 @@ describe 'grafana' do
       end
       context 'with default values' do
         it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('grafana') }
         it { is_expected.to contain_anchor('grafana::begin') }
         it { is_expected.to contain_class('grafana::params') }
         it { is_expected.to contain_class('grafana::install') }
