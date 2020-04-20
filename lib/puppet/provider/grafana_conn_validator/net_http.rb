3eeff2bcb3885d10362c0549e3296c858d28fb59
Trey Dockendorf <tdockendorf@osc.edu>
2020-04-20 15:52:45 -0400

    Remove misleading comments

--- lib/puppet/provider/grafana_conn_validator/net_http.rb
+++ lib/puppet/provider/grafana_conn_validator/net_http.rb
@@ -1,8 +1,3 @@
-# See: #10295 for more details.
-#
-# This is a workaround for bug: #4248 whereby ruby files outside of the normal
-# provider/type path do not load until pluginsync has occured on the puppetmaster
-#
 # In this case I'm trying the relative path first, then falling back to normal
 # mechanisms. This should be fixed in future versions of puppet but it looks
 # like we'll need to maintain this for some time perhaps.
